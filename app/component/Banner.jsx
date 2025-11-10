95650a6d96d89ef71026cc0271ddc5de5e30d81d
Zakir123164 <jkrhossain8@gamil.com>
2025-11-10 09:41:57 +0600

    car

---
+++ app/component/Banner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>This is banner part</div>
+  )
+}
+
+export default Banner
---
+++ app/component/Navbar.jsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+       <section id='Navbar'>
+            <div className='container'>
+           <div className="navRow flex justify-between items-center">
+
+                <div className="navLogo"> <Link href={"/"}><img src="/LuxuryCars.png" alt="logo" /></Link></div>
+         <div className="navItem ">
+            <ul className='flex gap-[25px]'>
+                <li><Link href={"/"}>Home</Link></li>
+                 <li><Link href={"/Services"}>Services</Link></li>
+                <li><Link href={"/Blog"}>Blog</Link></li>
+                <li><Link href={"/Contact Us"}>Contact Us</Link></li>
+            </ul>
+        </div>
+        <div className="navIcon">
+           <h2><Link href={"/sign In"}>Sign In</Link></h2>
+        </div>
+            </div>
+           </div>
+       </section>
+    </>
+  )
+}
+
+export default Navbar
