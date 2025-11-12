448fc7cccd0be1d763e919ebb8eaf50a51421276
Zakir123164 <jkrhossain8@gamil.com>
2025-11-12 10:54:10 +0600

    change

--- app/component/Banner.jsx
+++ app/component/Banner.jsx
@@ -4,8 +4,8 @@ const Banner = () => {
   return (
     <>
        <section id='banner'>
+           <div className="bnnerImage">
          <div className="bannerPart">
-           <div className="bnnerImage"><img src="/banner lambo 1.png" alt="" />
             <h2 className=''>safer ,faster and comfortable</h2>
             <p className=''>Get in touch with ourluxury cars</p>
             <button className=''>Rent Now</button>
