82c5d7431a65e18141e646a59261ec743a288668
Zakir123164 <jkrhossain8@gmail.com>
2025-11-13 01:28:03 +0600

    car

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from './component/Banner'
+import Book from './component/Book'
 
 const page = () => {
   return (
     <>
      <Banner/>
+     <Book/>
     </>
   )
 }
